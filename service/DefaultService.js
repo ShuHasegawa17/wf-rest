67a8ebed6a04f47f80f9309309f4a98466460c9b
shuhasegawa <hasegawa@abc.co.jp>
2022-02-20 17:01:52 +0900

    ユーザ一覧を追加

--- service/DefaultService.js
+++ service/DefaultService.js
@@ -13,13 +13,58 @@ exports.findUsers = function(tags,limit) {
     var examples = {};
     examples['application/json'] = [ {
   "id" : "1",
-  "name" : "taro",
-  "age" : "30",
+  "name" : "テスト太郎",
+  "userId" : "test01",
   "status" : "active"
 }, {
   "id" : "2",
-  "name" : "taro",
-  "age" : "30",
+  "name" : "サンプル次郎",
+  "userId" : "test02",
+  "status" : "active"
+}, {
+  "id" : "3",
+  "name" : "ユーザ０１",
+  "userId" : "user01",
+  "status" : "active"
+}, {
+  "id" : "4",
+  "name" : "ユーザ０２",
+  "userId" : "user02",
+  "status" : "inactive"
+}, {
+  "id" : "5",
+  "name" : "ユーザ０３",
+  "userId" : "user03",
+  "status" : "pending"
+}, {
+  "id" : "6",
+  "name" : "ユーザ０４",
+  "userId" : "user04",
+  "status" : "active"
+}, {
+  "id" : "7",
+  "name" : "ユーザ０５",
+  "userId" : "user05",
+  "status" : "inactive"
+}, {
+  "id" : "8",
+  "name" : "ユーザ０６",
+  "userId" : "user06",
+  "status" : "pending"
+}, {
+  "id" : "9",
+  "name" : "ユーザ０７",
+  "userId" : "user07",
+  "status" : "active"
+}, {
+  "id" : "10",
+  "name" : "ユーザ０８",
+  "userId" : "user08",
+  "status" : "pending"
+}, {
+  "id" : "11",
+  "name" : "ユーザ０９",
+  "userId" : "user09",
   "status" : "active"
 } ];
     if (Object.keys(examples).length > 0) {
